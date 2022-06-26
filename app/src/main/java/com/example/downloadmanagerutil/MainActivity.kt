9d356cedb0bf6607d97df8f245b79ef027a6b712
Oweis Alomari <oweis.alomari@arabbank.com.jo>
2022-06-26 21:51:01 +0300

    check for permissions

--- app/src/main/java/com/example/downloadmanagerutil/MainActivity.kt
+++ app/src/main/java/com/example/downloadmanagerutil/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.downloadmanagerutil
 
+import android.Manifest
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var permissionUtils: PermissionsUtil
 
     private lateinit var downloadBtn: Button
     private lateinit var downloadManager: DownloadManagerUtil
@@ -12,8 +14,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        permissionUtils = PermissionsUtil.getInstance(this, this.activityResultRegistry)
+        lifecycle.addObserver(permissionUtils)
         downloadBtn = findViewById(R.id.button)
         downloadManager = DownloadManagerUtil.newInstance(this, "Demo PDF")
-        downloadBtn.setOnClickListener { downloadManager.downloadPdfFile("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf") }
+        downloadBtn.setOnClickListener {
+            permissionUtils.requestPermission(
+                arrayOf(Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                object : PermissionsUtil.PermissionsListenerCallback {
+                    override fun onPermissionGranted() {
+                        downloadManager.downloadPdfFile("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+                    }
+
+                    override fun onPermissionDialogCancel() {}
+                })
+        }
     }
 }
